602176ff4f6bfb72fd6d7940fd119c2c4c1005d0
maxitucu <darrechithiago20@gmail.com>
2024-03-11 18:26:13 +0100

    mensaje del commit

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'gestion'
+
+mysql = MySQL(app)
