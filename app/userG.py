cf61d11f954825f1c476d779a85fefd6fb8767ba
Mateo Daniel Soliz Rueda <mateo@cometalabs.io>
2024-03-11 17:47:34 +0100

    asdasd

--- app/userG.py
+++ app/userG.py
@@ -14,3 +14,14 @@ class User(UserMixin):
     
     def check_password(self, password):
         return check_password_hash(self.password, password)
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
+    
+users = []
+
+def get_user(email):
+    for user in users:
+        if user.email == email:
+            return user
+    return None
