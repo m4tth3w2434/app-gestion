b58e22b71196c9c39e27589a44805693fa4b4b7a
Omi Gonzalez Amengual <omigonzalezamengual@gmail.com>
2024-03-15 18:55:10 +0100

    branch omi

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, url_parse
 from forms import SignupForm, LoginForm
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 from userG import users, get_user
@@ -40,5 +40,31 @@ def signup():
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # si el usuario ya ha hecho login redirigimos a index
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    # inicializamos el formulario
+    form = LoginForm()
+    # si el usuario hace submit:
+    if form.validate_on_submit():
+        # comprobamos que el email está en la base de datos:
+        user = get_user(form.email.data)
+        if user is not None and user.check_password(form.password.data):
+        # si el usuario existe y la contraseña es correcta llamamos
+        # la función que loguea al usuario y vemos si ha clicado en "recordarme"
+            login_user(user, remember= form.remember_me.data)
+            # hacemos un next_page para enviar a un sitio u otro
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                # si se cumple que todo está correcto redireccionamos a
+                next_page = url_for('index')
+            else:
+                # si no ha ido bien, volvemos a mostrar el login
+                next_page = url_for('login')
+
+            return redirect(next_page)
+            
 app.run(debug=True)
 
